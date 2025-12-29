409c7fc6e520690e811a654420fb3018f1ae408b
Thayne <legot1@gmail.com>
2025-12-29 13:18:53 -0700

    Update __init__.py

--- components/phase_config/__init__.py
+++ components/phase_config/__init__.py
@@ -1,7 +1,30 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
+import esphome.configvalidation as cv
+from esphome.components import sensor
+from esphome.const import CONFID
 
-CONFIG_SCHEMA = cv.Schema({})
+phaseconfigns = cg.esphomens.namespace("phaseconfig")
+PhaseConfig = phaseconfigns.class("PhaseConfig", cg.Component)
 
-def to_code (config):
-  cg.add_global(cg.RawExpression('#include "esphome/components/phase_config/phase_config.h"'))
+CONFOVERALLVOLTAGE = "overallvoltage"
+CONFPHASEABVOLTAGE = "phaseabvoltage"
+# Add more later if you want: phasebcvoltage, phasecavoltage, etc.
+
+CONFIGSCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declareid(PhaseConfig),
+        cv.Required(CONFOVERALLVOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONFPHASEABVOLTAGE): cv.useid(sensor.Sensor),
+    }
+).extend(cv.COMPONENTSCHEMA)
+
+async def tocode(config):
+    var = cg.newPvariable(config[CONFID])
+    await cg.registercomponent(var, config)
+
+    overall = await cg.getvariable(config[CONFOVERALLVOLTAGE])
+    cg.add(var.setoverallvoltage(overall))
+
+    if CONFPHASEABVOLTAGE in config:
+        ab = await cg.getvariable(config[CONFPHASEABVOLTAGE])
+        cg.add(var.setphaseabvoltage(ab))
