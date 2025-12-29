f0f875080912c2de23770d2d9893b30b532e2937
Thayne <legot1@gmail.com>
2025-12-29 05:41:06 -0700

    Create component.h

---
+++ esphome/components/emporia_vue_extended/component.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "esphome.h"
+#include "phase_config.h"
+
+namespace esphome {
+namespace phase_config {
+
+class PhaseConfigComponent : public Component {
+ public:
+  void setup() override {}
+  void loop() override {}
+};
+
+}  // namespace phase_config
+}  // namespace esphome
